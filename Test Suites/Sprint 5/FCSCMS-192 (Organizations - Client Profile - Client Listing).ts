f78276c1df13acc32d3901001dc54619601122a0
Vjay Gomez <42166909+papapangg@users.noreply.github.com>
2021-12-20 18:04:45 +0800

    Update FCSCMS 192

---
+++ Test Suites/Sprint 5/FCSCMS-192 (Organizations - Client Profile - Client Listing).ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>FCSCMS-192 (Organizations - Client Profile - Client Listing)</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1956c527-efbc-423d-be88-6f24016ada15</testSuiteGuid>
+   <testCaseLink>
+      <guid>859e0642-7c77-4b3d-8a1d-6d78f780140d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-192/FCSCMS-1049</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bf89b6b5-e5cb-4967-b8b0-a8195eb65828</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-192/FCSCMS-1050</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2eaeccfe-6b03-48a8-9f70-0d5448efb7d4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-192/FCSCMS-1051</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4899aaaa-db98-4ce5-bc4a-03cf23892d52</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-192/FCSCMS-1052</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>

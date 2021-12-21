a6ebe776169d8b4d9c7a7a5131048d9671fc0c43
Vjay Gomez <42166909+papapangg@users.noreply.github.com>
2021-12-21 21:19:26 +0800

    Update for Sprint 5 new Nexterous Build

--- Test Suites/Sprint 5/FCSCMS-192 (Organizations - Client Profile - Client Listing).ts
+++ Test Suites/Sprint 5/FCSCMS-192 (Organizations - Client Profile - Client Listing).ts
@@ -15,36 +15,36 @@
       <guid>859e0642-7c77-4b3d-8a1d-6d78f780140d</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Sprint 5/FCSCMS-192/FCSCMS-1049</testCaseId>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-192/FCSCMS-1049-S5 (Admin user should be able to log in using valid credentials)</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>bf89b6b5-e5cb-4967-b8b0-a8195eb65828</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Sprint 5/FCSCMS-192/FCSCMS-1050</testCaseId>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-192/FCSCMS-1050-S5 (Click the Clients option from the left navigation panel)</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>2eaeccfe-6b03-48a8-9f70-0d5448efb7d4</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Sprint 5/FCSCMS-192/FCSCMS-1051</testCaseId>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-192/FCSCMS-1051-S5 (User should be able to view client grid)</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>4899aaaa-db98-4ce5-bc4a-03cf23892d52</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Sprint 5/FCSCMS-192/FCSCMS-1052</testCaseId>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-192/FCSCMS-1052-S5 (User should be able to view-edit client information)</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>154e8e52-7d8c-482c-8087-e8b2a654eb10</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Sprint 5/FCSCMS-192/FCSCMS-1053</testCaseId>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-192/FCSCMS-1053-S5 (User should be able to use the pagination widget)</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>c42c6fa6-aed8-47c2-b6e0-99d1b02a34c9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Sprint 5/FCSCMS-192/FCSCMS-1054</testCaseId>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-192/FCSCMS-1054-S5 (User should be able to export file by clicking export icon)</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
--- Test Suites/Sprint 5/FCSCMS-193 (Organizations - Client Profile - Add New Client).ts
+++ Test Suites/Sprint 5/FCSCMS-193 (Organizations - Client Profile - Add New Client).ts
@@ -15,60 +15,60 @@
       <guid>fae0c521-51de-4652-8611-c02d3b363a87</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1137</testCaseId>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1137-S5 (Admin user should be able to log in using valid credentials)</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>69e0368e-55c8-466c-ab8b-c6688f832743</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1138</testCaseId>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1138-S5 (Click the Clients option from the left navigation panel)</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>ae4a765f-b5e8-4548-9403-b724b9c2357c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1139</testCaseId>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1139-S5 (Click on the Add New Client button)</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>250d79d8-8279-432e-ab2c-409ac6a8473c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1140</testCaseId>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1140-S5 (User can view the name of the client at the title of the page)</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>d4b27705-3128-40c8-bcb6-e1ef31ea8aa3</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1141</testCaseId>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1141-S5 (Edit details on the Basic Information tab)</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>a4c45f76-ac48-404d-88f0-c8439eaaf5f0</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1142</testCaseId>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1142-S5 (Click the Save button for the Basic Information tab)</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>4886b6c9-c42f-4bce-8b85-41f7d6b78184</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1143</testCaseId>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1143-S5 (Click the Yes to confirm saving of all the Details)</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>1254cac8-a5d1-4fec-ab49-c966291ce884</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1144</testCaseId>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1144-S5 (Click the No button to terminate the saving of the details entered)</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>dd5b81b8-2302-48c8-94a1-b6246bc3e44a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1145</testCaseId>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1145-S5 (Click the Cancel button for the Basic Information tab)</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>21970b78-2409-40fd-8140-0f8ca0111471</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1146</testCaseId>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1146-S5 (Click the Yes button from Cancel Confirmation modal</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>

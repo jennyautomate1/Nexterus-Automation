8f1a8588c883d133e385f9c46632f91462d94b8a
Vjay Gomez <42166909+papapangg@users.noreply.github.com>
2021-12-27 14:50:47 +0800

    Updates for FCSCMS-193 Test Scripts

--- Test Suites/Sprint 5/TestSuiteSite Sample.ts
+++ Test Suites/Sprint 5/TestSuiteSite Sample.ts
@@ -95,4 +95,136 @@
       <isRun>true</isRun>
       <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1163-S5 (Should be able to click the yes button on the notification modal)</testCaseId>
    </testCaseLink>
+   <testCaseLink>
+      <guid>909d7e37-0d67-4db7-9af5-30fd92b1cf99</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1164-S5 (Click the No button to terminate the saving of the Newly added or edited Contacts)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>49f917c9-8256-4e01-b9d3-387ade2d7278</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1165-S5 (The user can click the Cancel button for the Contacts tab)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1ec219c2-a6ee-4edd-9a7c-e8953c1af87b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1166-S5 (Able to click Yes button on the Notification Modal)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1e0beea4-0dff-496b-8151-7b9928cdb93a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1167-S5 (The user can click the No button from Cancel Confirmation modal)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>885d744f-6e0f-4db0-b260-9890dd8b971a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1168-S5 (The user can click the Carriers tab)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>37a36f82-3e1e-40dc-8f13-4e4ebe7d1669</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1169-S5 (The user can add a new Carrier)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0348d8b9-298e-4e00-b6a5-a29210456b92</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1170-S5 (Edit Carriers Tab)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1107814d-3a80-47e8-a2dc-0394fdb9a6d6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1171-S5 (The user can click the Save button for the carrier tab)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c6c9d87e-d113-4284-8052-7b33b49c6f6e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1172-S5 (Click Yes on the Notification Modal)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>09d26bde-c18e-4128-bdc5-45a4370811fe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1173-S5 (Click No on the Notification Modal)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>86b055ea-ae67-4ad9-b66a-3660b908a3eb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1174-S5 (The user can click the Cancel button for the Carrier tab)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>09ba95c7-f93e-46c1-a2cf-5459397a583f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1175-S5 (The user can click the Yes button from Cancel Confirmation modal)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>514a7f01-a4be-484f-90f8-4b9a98017cf2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1176-S5 (The user can click the No button from Cancel Confirmation modal)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b1f555a6-b685-45ee-97a1-f0adbdf862c3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1177-S5 (The user can click the Products tab)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3b50cfd6-39c0-445d-88ce-3775bc707159</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1178-S5 (The user can add a new Product)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2d2e2307-9e58-4031-a0f9-a0e551934e12</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1179-S5 (The user can vew or edit details of an existing Products on the Products tab)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>974cb468-01a9-4243-afe7-c4c3e59dff4f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1180-S5 (The user can click the Save button for the Products tab)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4d0e50cb-7cfe-4fa2-a4a4-a2f80a01fed6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1181-S5 (Click Yes button)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>43f47bbf-f10a-4118-92f3-fa591baf1e59</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1182-S5 (Click no button)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4eac8533-8168-4c33-a879-d833483da8f8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1183-S5 (The user can click the Product button for the Products tab)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b444b35b-b860-4ca0-b5c6-5e6943abe200</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1184-S5 (The user can click the Yes button from Cancel Confirmation modal)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b6f9b568-a046-4333-8b3b-817aed3ff10d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sprint 5/FCSCMS-193/FCSCMS-1185-S5 (The user can click the No button from Cancel Confirmation modal)</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
